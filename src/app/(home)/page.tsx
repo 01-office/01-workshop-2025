8becae52515973f8abd8dec5a9885d88c71a0972
강동혁 <kdnhyk@gmail.com>
2025-10-25 11:45:11 +0900

    feat: home 페이지 추가

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="h-full flex items-center">
+      <div className="grid grid-cols-2 w-full p-4">
+        <div>
+          <h1>0에서 1까지</h1>
+          <p>Portfolio Website Workshop</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
